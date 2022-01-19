73856f4902ced2477709370fc1e9b443eaf9fbbe
yoshihiro muraji <6unyon21sdvx@gmail.com>
2022-01-19 10:09:05 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <AnotherWorld />
+</template>
+
+<script>
+import AnotherWorld from "./components/AnotherWorld.vue";
+export default {
+  name: "App",
+  components: {
+    AnotherWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
